8ff2f084b34c99d1614e873e5dad177d4737d910
Ed <ed@wearenet.co.uk>
2022-07-07 15:47:45 +0100

    deleted superfluous console log

--- models/models.js
+++ models/models.js
@@ -197,7 +197,6 @@ exports.removeComment = (comment_id) => {
     return db
         .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *`, [comment_id])
         .then(({ rows }) => {
-            console.log(rows);
             if (rows.length === 0) {
                 return Promise.reject({
                     status: 400,
